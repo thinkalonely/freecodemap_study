b7cba0f6cf0f63423951a0427c70bb81bdcb5143
thinkalonely <thinkalonely@gmail.com>
2017-07-03 15:07:48 +0800

    arrays in JSON

---
+++ Arrays in JSON.js
@@ -0,0 +1,23 @@
+// 初始化变量
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// 请只修改这条注释以下的代码
+
+var secondTree = myPlants[1].list[1]; // 请修改这一行
